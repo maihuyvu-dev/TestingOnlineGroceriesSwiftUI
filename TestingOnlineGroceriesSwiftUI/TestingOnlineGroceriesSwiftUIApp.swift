fc50f55766a272532f6ee90de0fdcc4d545af580
Mai Huy Vu <huyvu06092003@gmail.com>
2026-01-21 11:11:21 +0700

    Initial Commit

---
+++ TestingOnlineGroceriesSwiftUI/TestingOnlineGroceriesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingOnlineGroceriesSwiftUIApp.swift
+//  TestingOnlineGroceriesSwiftUI
+//
+//  Created by Mai huy vu on 21/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TestingOnlineGroceriesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
